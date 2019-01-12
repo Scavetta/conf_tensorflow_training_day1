757f9ab5d47e34b2c0631b2e8f8f27373d3e0ecf
Rick Scavetta <milou@MBP-de-Rick.attlocal.net>
2019-01-12 15:30:02 -0600

    update session 2

--- 1_Deep_Learning_Intro/scripts/Boston_Z.R
+++ 1_Deep_Learning_Intro/scripts/Boston_Z.R
@@ -8,3 +8,6 @@ mean <- colMeans(train_data) # mean of each column
 std <- apply(train_data, 2, sd) # stdev of each column
 train_data <- scale(train_data, center = mean, scale = std)
 test_data <- scale(test_data, center = mean, scale = std)
+
+rm(mean)
+rm(std)
