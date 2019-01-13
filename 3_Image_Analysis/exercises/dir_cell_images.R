0d75315e73702587e97b45b7cb23a529815e0d86
scavetta <office@scavetta.academy>
2019-01-13 01:34:59 +0000

    Image Analysis

---
+++ 3_Image_Analysis/exercises/dir_cell_images.R
@@ -0,0 +1,21 @@
+# Data Directories for cell_images
+
+base_dir <- "~/data/cell_images"
+
+# Train sets:
+train_dir <- file.path(base_dir, "train")
+train_parasite_dir <- file.path(train_dir, "parasite")
+train_uninfected_dir <- file.path(train_dir, "uninfected")
+
+# Validation sets:
+validation_dir <- file.path(base_dir, "validation")
+validation_parasite_dir <- file.path(validation_dir, "parasite")
+validation_uninfected_dir <- file.path(validation_dir, "uninfected")
+
+# Test sets:
+test_dir <- file.path(base_dir, "test")
+test_parasite_dir <- file.path(test_dir, "parasite")
+test_uninfected_dir <- file.path(test_dir, "uninfected")
+
+
+
---
+++ 3_Image_Analysis/exercises/dir_labradoodle_friedchicken.R
@@ -0,0 +1,21 @@
+# Data Directories for labradoodle_friedchicken
+
+base_dir <- "~/data/labradoodle_friedchicken"
+
+# Train sets:
+train_dir <- file.path(base_dir, "train")
+train_chicken_dir <- file.path(train_dir, "chicken")
+train_dogs_dir <- file.path(train_dir, "dog")
+
+# Validation sets:
+validation_dir <- file.path(base_dir, "validation")
+validation_chicken_dir <- file.path(validation_dir, "chicken")
+validation_dogs_dir <- file.path(validation_dir, "dog")
+
+# Test sets:
+test_dir <- file.path(base_dir, "test")
+test_chicken_dir <- file.path(test_dir, "chicken")
+test_dog_dir <- file.path(test_dir, "dog")
+
+
+
